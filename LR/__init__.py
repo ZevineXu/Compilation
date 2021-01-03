efda1e46b43a31234024495cee8142fb518e1a29
JohnXu <1808212297@qq.com>
2021-01-03 19:17:27 +0800

    添加终结符非终结符判定，生成follow和first集

---
+++ LR/__init__.py
@@ -0,0 +1,16 @@
+# action_di = {"id":0,"+":1,"*":2,"(":3,")":4,"$":5}
+# goto_di = {"E":0,"T":1,"F":2}
+
+# pattern_all = "id|create|table|[-/+*A-Za-z~|][']?|[(),]|·"
+# pattern_ter = "[+*/]|-|table|create|[(),]"
+
+# todo: 正则表达 end
+pattern_all = "select|from|where|=|id|insert into|values|create|table|[-/+*A-Za-z~][']?|[(),;]|~|·"
+pattern_ter = "select|from|where|=|insert into|values|id|table|create|[,();]|~"
+
+
+
+# action_di = {"id":0,"+":1,"*":2,"(":3,")":4,"-":5,"/":6,'$':7}
+# goto_di = {"E":0,"T":1,"F":2}
+
+
